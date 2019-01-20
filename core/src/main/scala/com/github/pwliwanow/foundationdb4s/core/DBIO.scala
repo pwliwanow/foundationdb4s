ef329ddd3d227ad5d5ab3377ae42396f3793784e
paweliwanow <pwliwanow@users.noreply.github.com>
2019-01-20 13:34:28 +0100

    #26 Make it possible to execute transactions using just TransactionContext and ExecutionContextExecutor

--- core/src/main/scala/com/github/pwliwanow/foundationdb4s/core/DBIO.scala
+++ core/src/main/scala/com/github/pwliwanow/foundationdb4s/core/DBIO.scala
@@ -3,7 +3,7 @@ package com.github.pwliwanow.foundationdb4s.core
 import java.util.concurrent.{CompletableFuture, CompletionException}
 
 import cats.{Monad, StackSafeMonad}
-import com.apple.foundationdb.Transaction
+import com.apple.foundationdb.{Transaction, TransactionContext}
 import com.apple.foundationdb.tuple.Versionstamp
 
 import scala.compat.java8.FutureConverters._
@@ -26,14 +26,25 @@ final case class DBIO[+A](
     * In some cases client can be unable to determine whether a transaction succeeded.
     * In these cases, your transaction may be executed twice.
     * For more information see:
-    * https://apple.github.io/foundationdb/administration.html#administration-running-foundationdb#transactions-with-unknown-results
+    * https://apple.github.io/foundationdb/developer-guide.html#transactions-with-unknown-results
     *
     * Any error encountered when executing DBIO will be set on the resulting Future.
     *
     * @return a [[Future]] that will contain a value returned by running this DBIO
     */
   def transact(transactor: Transactor): Future[A] = {
-    transactVersionstamped(transactor).map { case (x, _) => x }(transactor.ec)
+    transact(transactor.db)(transactor.ec)
+  }
+
+  /** Runs DBIO within provided [[TransactionContext]].
+    *
+    * Depending on the type of context, this may execute the supplied function multiple times
+    * if an error is encountered.
+    *
+    * @return a [[Future]] that will contain a value returned by running this DBIO
+    */
+  def transact(context: TransactionContext)(implicit ec: ExecutionContextExecutor): Future[A] = {
+    transactVersionstamped(context).map { case (x, _) => x }
   }
 
   /** Runs a transactional DBIO with retry logic in a non-blocking way.
@@ -54,10 +65,35 @@ final case class DBIO[+A](
     */
   def transactVersionstamped(
       transactor: Transactor,
-      userVersion: Int = 0): Future[(A, Option[Versionstamp])] = {
-    implicit val ec = transactor.ec
+      userVersion: Int): Future[(A, Option[Versionstamp])] = {
+    transactVersionstamped(transactor.db, userVersion)(transactor.ec)
+  }
+
+  def transactVersionstamped(transactor: Transactor): Future[(A, Option[Versionstamp])] = {
+    transactVersionstamped(transactor.db)(transactor.ec)
+  }
+
+  def transactVersionstamped(context: TransactionContext)(
+      implicit ec: ExecutionContextExecutor): Future[(A, Option[Versionstamp])] = {
+    transactVersionstamped(context, userVersion = 0)
+  }
+
+  /** Runs DBIO within provided [[TransactionContext]].
+    *
+    * Depending on the type of context, this may execute the supplied function multiple times
+    * if an error is encountered.
+    *
+    * @return a [[Future]] that will contain a tuple of value returned by running this DBIO and
+    *         an optional [[Versionstamp]].
+    *         Returned [[Versionstamp]] will be empty if executing this DBIO did not modify the database
+    *         (e.g. when running this function on `DBIO.pure("value")`).
+    *         For DBIO that modified the database, [[Versionstamp]] will be equal to the versionstamp used
+    *         by any versionstamp operations in this DBIO.
+    */
+  def transactVersionstamped(context: TransactionContext, userVersion: Int)(
+      implicit ec: ExecutionContextExecutor): Future[(A, Option[Versionstamp])] = {
     val promisedVersionstamp = Promise[Option[Versionstamp]]
-    val futureRes: Future[A] = transactor.db
+    val futureRes: Future[A] = context
       .runAsync(
         (tx: Transaction) => {
           val futureMaybeVersionstamp =
