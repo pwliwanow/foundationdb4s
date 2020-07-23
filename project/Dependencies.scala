f9de60ff2bc270b239bf678f90c6d132e0de880b
Scala Steward <me@scala-steward.org>
2020-07-24 00:41:54 +0200

    Update mockito-core to 3.4.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
     val java8CompatVersion = "0.9.1"
     val java8Compat = "org.scala-lang.modules" %% "scala-java8-compat" % java8CompatVersion
 
-    val mockitoVersion = "3.4.2"
+    val mockitoVersion = "3.4.4"
     val mockito = "org.mockito" % "mockito-core" % mockitoVersion
 
     val scalaTestVersion = "3.2.0"
